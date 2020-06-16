bioinfo-pf-curie/geniac

commit 14886e25b4066dae6e17b1b12deacba4a63e7aa1

[MODIF] fix issue #100

install/docker.nf
@@ -285,7 +285,7 @@ process buildDockerRecipeFromSourceCode {
 }
 
 onlyCondaRecipeCh = dockerRecipeCh3.mix(dockerRecipeCh4)
-dockerAllRecipeCh = dockerRecipeCh1.mix(dockerRecipeCh2).mix(onlyCondaRecipeCh).mix(dockerRecipeCh5)
+dockerAllRecipeCh = dockerRecipeCh1.mix(dockerRecipeCh2).mix(onlyCondaRecipeCh).mix(dockerRecipeCh5).dump(tag:'dockerRecipes')
 
 process buildImages {
     tag "${key}"
@@ -296,14 +296,14 @@ process buildImages {
 
     input:
     set val(key), file(dockerRecipe), val(optionalPath) from dockerAllRecipeCh
-    file condaYml from condaRecipes.collect()
-    file fileDep from fileDependencies.collect()
-    file moduleDir from sourceCodeDirCh.collect()
+    file condaYml from condaRecipes.collect().ifEmpty([])
+    file fileDep from fileDependencies.collect().ifEmpty([])
+    file moduleDir from sourceCodeDirCh.collect().ifEmpty([])
 
     script:
-
+    excludemoduleDir = moduleDir == [] ? "" : "--exclude='modules'"
     """
-    tar cvfh contextDir.tar *
+    tar cvfh contextDir.tar ${excludemoduleDir} *
     mkdir contextDir
     tar xvf contextDir.tar --directory contextDir
     docker build  -f ${dockerRecipe} -t ${key.toLowerCase()} contextDir

.gitignore
@@ -8,3 +8,4 @@
 utils/docs/_build/
 .idea/
 .vscode/
+/docs/_build/

commit 8040be5345aa7f8e0ff578ece09cce9a219ec462

[BUGFIX] correct bug module dir check introduced with commit 14886e25 (issue #100)

install/docker.nf
@@ -301,7 +301,7 @@ process buildImages {
     file moduleDir from sourceCodeDirCh.collect().ifEmpty([])
 
     script:
-    excludemoduleDir = moduleDir == [] ? "" : "--exclude='modules'"
+    excludemoduleDir = moduleDir == [] ? "--exclude='modules'" : ""
     """
     tar cvfh contextDir.tar ${excludemoduleDir} *
     mkdir contextDir