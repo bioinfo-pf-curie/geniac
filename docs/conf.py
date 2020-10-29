b5c9ed717540d74efcbef9c6b6e95df0f0dee3c1
Philippe Hupé <philippe.hupe@curie.fr>
2020-10-29 11:09:47 +0100

    [MODIF] logo geniac

--- docs/conf.py
+++ docs/conf.py
@@ -85,6 +85,9 @@ todo_include_todos = False
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = ["_themes", ]
+html_logo = 'images/geniac-logo.svg'
+html_favicon = 'images/geniac-logo.png'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
