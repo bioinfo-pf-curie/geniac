264242165561ce529710741d6a14fbf6c6e87f06
Fabrice Allain <fabrice.allain@curie.fr>
2020-12-09 17:29:46 +0100

    [MODIF] add lint command skeleton (#133)

--- src/geniac/check.py
+++ src/geniac/check.py
@@ -16,6 +16,63 @@ _logger = logging.getLogger(__name__)
 class GCheck(GCommand):
     """Linter command for geniac"""
 
+
+    def check_config_file_content(self):
+        """Check the structure of the repo
+
+        Returns:
+
+        """
+        pass
+
+    def get_labels_from_folders(self):
+        """Parse information from recipes and modules folders
+
+        Returns:
+
+        """
+        pass
+
+    def get_labels_from_main(self):
+        """Parse only the main.nf file
+
+        Returns:
+
+        """
+        pass
+
+    def get_labels_from_process_config(self):
+        """Parse only the conf/process.config
+
+        Returns:
+
+        """
+        pass
+
+    def check_labels(self):
+        """
+
+        Returns:
+
+        """
+        pass
+
+    def check_dependencies_dir(self):
+        """
+
+        Returns:
+
+        """
+        pass
+
+    def check_env_dir(self):
+        """
+
+        Returns:
+
+        """
+        pass
+
     def run(self):
         """Execute the main routine
 
@@ -23,3 +80,10 @@ class GCheck(GCommand):
 
         """
         pass
+        self.check_config_file_content()
+        self.get_labels_from_folders()
+        self.get_labels_from_main()
+        self.get_labels_from_process_config()
+        self.check_labels()
+        self.check_dependencies_dir()
+        self.check_env_dir()
