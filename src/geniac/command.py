fc7e5373c1008e330f8fc976c310827773b244c9
Fabrice Allain <fabrice.allain@curie.fr>
2020-12-09 17:25:40 +0100

    [MODIF] refactor command.py properties (#133)

--- src/geniac/command.py
+++ src/geniac/command.py
@@ -6,6 +6,7 @@
 import logging
 from abc import ABC, abstractmethod
 from configparser import ConfigParser, ExtendedInterpolation
+from os import getcwd
 from os.path import isdir, isfile
 
 from pkg_resources import resource_stream
@@ -28,9 +29,9 @@ class GCommand(ABC):
             project_dir (str): path to the Nextflow Project Dir
             config_file (str): path to a configuration file (INI format)
         """
-        self._project_dir = project_dir if project_dir and isdir(project_dir) else None
-        self._config_file = config_file if config_file and isfile(config_file) else None
-        self._config = self.load_config()
+        self.project_dir = project_dir
+        self.config_file = config_file
+        self.config = self.load_config(self.config_file)
 
     def load_config(self, config_file=None):
         """Load default configuration file and update option with config_file
@@ -38,11 +39,16 @@ class GCommand(ABC):
         Returns:
             :obj:`configparser.ConfigParser`: config instance
         """
-        config = ConfigParser(interpolation=ExtendedInterpolation(), allow_no_value=True)
-        # TODO: add defauldict in order to init tree. sections with required, path, files, opt_files and exclude keys
+        config = ConfigParser(
+            interpolation=ExtendedInterpolation(), allow_no_value=True
+        )
+        # TODO: add defauldict in order to init tree. sections with required, path,
+        #  files, opt_files and exclude keys
         # TODO: remove has_section checks in GCheck after previous todo
         # Read default config file
-        config.read_string(resource_stream(__name__, self.default_config).read().decode())
+        config.read_string(
+            resource_stream(__name__, self.default_config).read().decode()
+        )
         if config_file:
             # Read configuration file
             config.read(config_file)
@@ -57,6 +63,25 @@ class GCommand(ABC):
         """
         return self._project_dir
 
+    @project_dir.setter
+    def project_dir(self, value):
+        """If value is not a directory, set the project dir to the current directory"""
+        self._project_dir = value if value and isdir(value) else getcwd()
+
+    @property
+    def config_file(self):
+        """Configuration file (optional)
+
+        Returns:
+
+        """
+        return self._config_file
+
+    @config_file.setter
+    def config_file(self, value):
+        """"""
+        self._config_file = value if value and isfile(value) else None
+
     @property
     def config(self):
         """ConfigParser property
@@ -65,6 +90,12 @@ class GCommand(ABC):
             :obj:`configparser.ConfigParser`: config instance
         """
         return self._config
+
+    @config.setter
+    def config(self, value):
+        """Update base dir with project dir"""
+        value.set("tree.base", "path", self.project_dir)
+        self._config = value
 
     @abstractmethod
     def run(self):
