4ea55f09a0b8b94cd562c610be83ab4c0a64e33e
Fabrice Allain <fabrice.allain@curie.fr>
2021-01-20 17:29:50 +0100

    [ADD] add base tests (#133)

---
+++ tests/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""test_base.py: Test geniac.base module"""
+
+from configparser import ConfigParser
+from pathlib import Path
+
+import pytest
+
+from geniac.base import GBase
+
+__author__ = "Fabrice Allain"
+__copyright__ = "Institut Curie 2020"
+
+
+@pytest.fixture
+def default_gbase():
+    """Define a GCommand instance for testing"""
+    return GBase()
+
+
+def test_gbase(default_gbase):
+    """Check if default GConfor has been instantiated correctly with all default GBase properties"""
+    assert default_gbase.project_dir == Path().cwd()
+    assert default_gbase.config_file is None
+    assert isinstance(default_gbase.config, ConfigParser)
