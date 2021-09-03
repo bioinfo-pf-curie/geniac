aae8fc828f560d7e0e6a03eda7772e52ccee1969
Philippe Hupé <philippe.hupe@curie.fr>
2021-09-03 22:33:51 +0200

    [MODIF] geniac lint: modules folder changed to modules/fromSource in geniac.ini (Issue #207)

--- src/geniac/commands/check.py
+++ src/geniac/commands/check.py
@@ -31,7 +31,7 @@ class GCheck(GCommand):
     )
     MODULE_CMAKE_RE_TEMP = (
         r"ExternalProject_Add\(\s*{label}[\s\w_${{}}\-/=]*SOURCE_"
-        r"DIR +\$\{{pipeline_source_dir\}}/modules/{label}"
+        r"DIR +\$\{{pipeline_source_dir\}}/modules/fromSource/{label}"
     )
     SINGULARITY_DEP_RE_TEMP = r"\%files\s+{dependency} [\/\w.]+{dependency}"
     DOCKER_DEP_RE_TEMP = r"ADD +{dependency} [\/\w.]+{dependency}"
