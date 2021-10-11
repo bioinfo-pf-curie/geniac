bioinfo-pf-curie/geniac

commit aa4b6babe187358579992cfe89c884b84ac4dc88

[MODIF] fix unecessary warnings (#235)

src/geniac/commands/check.py
@@ -212,7 +212,20 @@ class GCheck(GCommand):
                         tree_section, "mandatory", lazy_glob=True
                     ),
                     # Path(s) to optional file(s)
-                    "optional_files": self.get_config_path(tree_section, "optional"),
+                    "optional_files": list(
+                        set(
+                            [
+                                path
+                                for path in self.get_config_path(
+                                    tree_section, "optional"
+                                )
+                                if path
+                                not in self.get_config_path(
+                                    tree_section, "mandatory", lazy_glob=True
+                                )
+                            ]
+                        )
+                    ),
                     # Path(s) to file(s) excluded from the analysis
                     "excluded_files": self.get_config_path(tree_section, "excluded"),
                     # Path(s) to file(s) excluded from the analysis
@@ -549,15 +562,29 @@ class GCheck(GCommand):
                     "path": self.get_config_path(
                         GCheck.GENIAC_CHECK_CONFIG,
                         default_config_name,
-                        single_path=True,
+                        single_path=default_config_name,
                     ),
                     "check_config": getattr(
                         self, f"_check_{default_config_name}_config", None
                     ),
                 },
             )
-            for default_config_name in self.default_config.options(
+            for default_config_name in self.get_config_section_items(
                 GCheck.GENIAC_CHECK_CONFIG
+            )
+            if default_config_name != "all"
+        ) | OrderedDict(
+            (
+                default_config_path.stem,
+                {
+                    "path": default_config_path,
+                    "check_config": getattr(
+                        self, f"_check_{default_config_path.stem}_config", None
+                    ),
+                },
+            )
+            for default_config_path in self.get_config_path(
+                GCheck.GENIAC_CHECK_CONFIG, "all", single_path=False
             )
         )
 
@@ -577,6 +604,18 @@ class GCheck(GCommand):
             )
         ]
 
+        default_config_paths = {
+            config_type: (
+                self.get_config_path(
+                    ".".join([GCheck.TREE_SUFFIX, "base"]), config_type
+                )
+                + self.get_config_path(
+                    ".".join([GCheck.TREE_SUFFIX, "conf"]), config_type
+                )
+            )
+            for config_type in ("mandatory", "optional")
+        }
+
         # For each Nextflow configuration file analyzed
         for config_key, project_config_scope in project_config_scopes.items():
             project_config_path = project_config_scope["path"]
@@ -588,18 +627,6 @@ class GCheck(GCommand):
                 "conda_check": self.default_config.getboolean(
                     self.GENIAC_FLAGS, "condaCheck"
                 ),
-            }
-
-            default_config_paths = {
-                config_type: (
-                    self.get_config_path(
-                        ".".join([GCheck.TREE_SUFFIX, "base"]), config_type
-                    )
-                    + self.get_config_path(
-                        ".".join([GCheck.TREE_SUFFIX, "conf"]), config_type
-                    )
-                )
-                for config_type in ("mandatory", "optional")
             }
 
             # If the project config file does not exists and does not belong to default
@@ -617,7 +644,8 @@ class GCheck(GCommand):
             # Read the Nextflow configuration file
             self.nxf_config.read(
                 project_config_path,
-                warnings=project_config_path in default_config_paths.get("mandatory"),
+                warnings=config_key
+                not in self.default_config.options(GCheck.GENIAC_CHECK_CONFIG),
             )
             if config_method:
                 self.info(

src/geniac/conf/geniac.ini
@@ -22,14 +22,14 @@ env          = env
 modules      = modules.fromSource
 
 # Nextflow configuration default files analyzed by the linter
-# NB: other config files found in geniac.directories.conf will also be checked
 [geniac.check.config]
-nextflow     = ${tree.base:path}/nextflow.config
+nextflow      = ${tree.base:path}/nextflow.config
 base         = ${tree.conf:path}/base.config
 process      = ${tree.conf:path}/process.config
 geniac       = ${tree.conf:path}/geniac.config
 genomes      = ${tree.conf:path}/genomes.config
 test         = ${tree.conf:path}/test.config
+all          = ${tree.conf:path}/**/*.config
 
 # Files generated by geniac. They are normally included in Nextflow profiles
 [geniac.generated.config]
@@ -62,6 +62,7 @@ recursive   = false
 path        = .
 # Path(s) to mandatory file(s)
 mandatory   =
+    ${project.workflow:main}
     ${geniac.check.config:nextflow}
 # Path(s) to optional file(s)
 optional
@@ -92,10 +93,13 @@ mandatory   =
     ${geniac.check.config:geniac}
     ${geniac.check.config:process}
 optional   =
+    ${geniac.check.config:all}
     ${geniac.check.config:test}
     ${geniac.check.config:genomes}
-    ${tree.conf:path}/**/*.config
 excluded    =
+    ${tree.conf:path}/*.md
+    ${tree.conf:path}/*.rst
+    ${tree.conf:path}/*.txt
     ${geniac.generated.config:conda}
     ${geniac.generated.config:multiconda}
     ${geniac.generated.config:singularity}
@@ -113,7 +117,10 @@ recursive   = false
 path        = ${tree.base:path}/recipes
 mandatory
 optional
-excluded
+excluded    =
+    ${tree.recipes:path}/*.md
+    ${tree.recipes:path}/*.rst
+    ${tree.recipes:path}/*.txt
 prohibited
 
 # base/recipes/conda

commit ce3a91b154926ac62e3672ae18ed3afd2ac14f10

[MODIF] fix bug introduced with  aa4b6bab for warning8 (#235)

src/geniac/commands/check.py
@@ -560,6 +560,7 @@ class GCheck(GCommand):
             (
                 default_config_name,
                 {
+                    "expected": True,
                     "path": self.get_config_path(
                         GCheck.GENIAC_CHECK_CONFIG,
                         default_config_name,
@@ -574,10 +575,13 @@ class GCheck(GCommand):
                 GCheck.GENIAC_CHECK_CONFIG
             )
             if default_config_name != "all"
-        ) | OrderedDict(
+        )
+        project_config_scopes |= OrderedDict(
             (
                 default_config_path.stem,
                 {
+                    "expected": default_config_path.stem
+                    in self.get_config_section_items(GCheck.GENIAC_CHECK_CONFIG),
                     "path": default_config_path,
                     "check_config": getattr(
                         self, f"_check_{default_config_path.stem}_config", None
@@ -587,13 +591,8 @@ class GCheck(GCommand):
             for default_config_path in self.get_config_path(
                 GCheck.GENIAC_CHECK_CONFIG, "all", single_path=False
             )
+            if default_config_path.stem not in project_config_scopes
         )
-
-        # Configuration files analyzed
-        project_config_paths = [
-            project_config_scopes[config_scope]["path"]
-            for config_scope in project_config_scopes
-        ]
 
         # Generate path to configuration files produced by geniac
         generated_geniac_config_paths = [
@@ -605,17 +604,11 @@ class GCheck(GCommand):
             )
         ]
 
-        default_config_paths = {
-            config_type: (
-                self.get_config_path(
-                    ".".join([GCheck.TREE_SUFFIX, "base"]), config_type
-                )
-                + self.get_config_path(
-                    ".".join([GCheck.TREE_SUFFIX, "conf"]), config_type
-                )
-            )
-            for config_type in ("mandatory", "optional")
-        }
+        expected_geniac_config_paths = [
+            config_scope.get("path")
+            for config, config_scope in project_config_scopes.items()
+            if config_scope.get("expected")
+        ]
 
         # For each Nextflow configuration file analyzed
         for config_key, project_config_scope in project_config_scopes.items():
@@ -623,7 +616,7 @@ class GCheck(GCommand):
             config_method = project_config_scope["check_config"]
             config_args = {
                 "config_path": project_config_path,
-                "default_config_paths": project_config_paths,
+                "default_config_paths": expected_geniac_config_paths,
                 "default_geniac_files_paths": generated_geniac_config_paths,
                 "conda_check": self.default_config.getboolean(
                     self.GENIAC_FLAGS, "condaCheck"
@@ -633,9 +626,7 @@ class GCheck(GCommand):
             # If the project config file does not exists and does not belong to default
             # geniac files
             if not project_config_path.exists():
-                if project_config_path not in default_config_paths.get(
-                    "optional"
-                ) + default_config_paths.get("mandatory"):
+                if project_config_path not in expected_geniac_config_paths:
                     self.error(
                         "Nextflow config file %s does not exist.",
                         project_config_path.relative_to(self.project_dir),